bba92bd8bbb359587df40314dd9a52481afc37b0
Daniel <daniel@daniels-mbp-5.mynetworksettings.com>
2023-11-30 22:31:14 -0800

    adding docs

--- docs/conf.py
+++ docs/conf.py
@@ -85,7 +85,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -93,8 +93,8 @@ pygments_style = None
 #
 # html_theme_options = {}
 html_static_path = ["./source/_static"]
-#html_logo = "_static/.png"
-#html_theme_options = {"logo_only": True}
+html_logo = "./source/_static/logo.png"
+html_theme_options = {"logo_only": True}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
